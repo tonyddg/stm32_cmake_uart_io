b0c5a0faba588ef3c9c662ca4f527a345984c120
tonyddg <799052200@qq.com>
2024-02-05 22:33:08 +0800

    修改关于 uart io 项目的注释

--- user/inc/user_uart.h
+++ user/inc/user_uart.h
@@ -13,7 +13,7 @@
  * @return osStatus_t 插入队列执行结果, 当队列未正确初始化时, 将返回 osError
  * @note 使用该函数前, 任务 `UART1SendTask` 必须运行中  
  * @note 该函数为线程安全的, 建议使用此函数发送数据, 而非 HAL_UART_Transmit 
- * @example UART1SendData(ConstBuf_CreateByStr("Hello World\r\n"), osWaitForever);
+ * @example UART1SendData(ConstBuf_CreateByStr("Hello World\r\n", 0), osWaitForever);
  */
 osStatus_t UART1SendData(ConstBuf* data, uint32_t timeout);
 
